a96d598a12a19f04613891b09d5e4ee362df5835
ChrisHeo <christopher.s.heo@gmail.com>
2016-07-11 23:51:16 -0400

    minimal function for to do list

--- Client/app.js
+++ Client/app.js
@@ -3,5 +3,31 @@ console.log("APP.js connected");
 angular.module('zen', [])
   .controller('toDo', function($scope){
 
-    $scope.tasks = [];
+    $scope.list = [];
+    $scope.completed = [];
+
+    $scope.add = function() {
+      $scope.list.push($scope.task);
+      //clear input field
+      $scope.task = "";
+    };
+
+    $scope.done = function() {
+      var done = $scope.list.splice(this.$index, 1)[0];
+      $scope.completed.push(done);
+    };
+
+    //redo task, delete from completed
+    $scope.redo = function() {
+      var redo = $scope.completed.splice(this.$index, 1)[0];
+      $scope.list.push(redo);
+    };
+
+    $scope.deleteFromList = function(item) {
+      $scope.list.splice($scope.list.indexOf(item), 1);
+    };
+
+    $scope.deleteFromCompleted = function(item) {
+      $scope.completed.splice($scope.completed.indexOf(item), 1);
+    };
   });
