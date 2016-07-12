9c612c7cef3cf075013e348ba15d3e6582b8deef
ChrisHeo <christopher.s.heo@gmail.com>
2016-07-12 16:37:32 -0400

    version 1

---
+++ client/app.js
@@ -0,0 +1,35 @@
+console.log("APP.js connected");
+
+angular.module('zen', [])
+  .controller('toDoList', function($scope){
+
+    $scope.list = [];
+    $scope.completed = [];
+
+    $scope.add = function() {
+      if($scope.task){
+        $scope.list.push($scope.task);
+        //clear input field
+        $scope.task = "";
+      }
+    };
+
+    $scope.done = function() {
+      var done = $scope.list.splice(this.$index, 1)[0];
+      $scope.completed.push(done);
+    };
+
+    //redo task, delete from completed
+    $scope.redo = function() {
+      var redo = $scope.completed.splice(this.$index, 1)[0];
+      $scope.list.push(redo);
+    };
+
+    $scope.deleteFromList = function(item) {
+      $scope.list.splice($scope.list.indexOf(item), 1);
+    };
+
+    $scope.deleteFromCompleted = function(item) {
+      $scope.completed.splice($scope.completed.indexOf(item), 1);
+    };
+  });
